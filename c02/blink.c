46a17870f0583eed0c7ee88bde54ed2fded0463a
Marc van Heerden <11750833+marcvanheerden@users.noreply.github.com>
2024-12-02 14:31:54 +0200

    fix C day02

--- c02/blink.c
+++ c02/blink.c
@@ -10,17 +10,18 @@
 bool assess(int levels[], int length, int ignore) {
 
     int buffer = 0;
-    int offset = -1;
     bool started = false;
     int diffs[MAX_LEVELS - 1];
+    int diff_count = 0;
 
     for (int idx = 0; idx < length; idx++) {
         if (idx == ignore) {
-            offset = -2;
             continue;
         }
+
         if (started) {
-            diffs[idx + offset] = levels[idx] - buffer;
+            diffs[diff_count] = levels[idx] - buffer;
+            diff_count++;
         }
 
         buffer = levels[idx];
@@ -30,14 +31,14 @@ bool assess(int levels[], int length, int ignore) {
     bool sign = diffs[0] > 0;
 
     // check for monotonicity
-    for (int idx = 1; idx < (length - 1); idx++) {
-        if (sign != diffs[idx] > 0) {
+    for (int idx = 1; idx < diff_count; idx++) {
+        if (sign != (diffs[idx] > 0)) {
             return false;
         }
     }
 
     // check for change bounds
-    for (int idx = 0; idx < (length - 1); idx++) {
+    for (int idx = 0; idx < diff_count; idx++) {
         if (abs(diffs[idx]) < 1 || abs(diffs[idx]) > 3) {
             return false;
         }
@@ -50,8 +51,6 @@ int count_safe_levels(char input[], bool tolerant) {
     char *endptr;
     int level_count = 0;
     int safe_count = 0;
-    int buffer1 = 0;
-    int buffer2 = 0;
     int levels[MAX_LEVELS] = {0};
 
     while (*input != '\0') {
