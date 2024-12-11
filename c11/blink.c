7dac32ba237e92ecc6616764af2bd4c856bbf8cf
Marc van Heerden <11750833+marcvanheerden@users.noreply.github.com>
2024-12-11 12:17:38 +0200

    still not working C day11

--- c11/blink.c
+++ c11/blink.c
@@ -6,17 +6,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_NODES 200
+#define MAX_NODES 2000
 
 typedef struct {
-    uint64_t val;
-    short next;
+    unsigned long long val;
+    unsigned long long count;
 } Node;
 
-int count_digits(long long number) {
+int count_digits(unsigned long long number) {
     // invariant: number > 0
 
-    long long place = 10;
+    unsigned long long place = 10;
     int digits = 1;
 
     while (number >= place) {
@@ -27,10 +27,11 @@ int count_digits(long long number) {
     return digits;
 }
 
-void split(long long number, int digits, long long *front, long long *back) {
+void split(unsigned long long number, int digits, unsigned long long *front,
+           unsigned long long *back) {
     // invariant: number > 0, digits is even
 
-    long long div = 1;
+    unsigned long long div = 1;
     for (int zeros = 1; zeros <= digits / 2; zeros++) {
         div *= 10;
     }
@@ -39,7 +40,7 @@ void split(long long number, int digits, long long *front, long long *back) {
     *back = number - (*front * div);
 }
 
-long long part1(char input[]) {
+unsigned long long part1(char input[], int blinks) {
 
     Node nodepool[MAX_NODES];
     int freenode = 0;
@@ -52,51 +53,75 @@ long long part1(char input[]) {
         }
 
         Node node;
-        node.val = strtoll(cursor, &endptr, 10);
+        node.val = strtoull(cursor, &endptr, 10);
         cursor = endptr;
-        node.next = -1;
-        if (freenode > 0) {
-            nodepool[freenode - 1].next = freenode;
-        }
+        node.count = 1;
+
         nodepool[freenode] = node;
         freenode++;
     }
 
-    for (int _blink = 0; _blink < 25; _blink++) {
-        int node_idx = 0;
-        while (node_idx != -1) {
+    for (int _blink = 0; _blink < blinks; _blink++) {
+        int frozen_node_count = freenode;
+        for (int node_idx = 0; node_idx < frozen_node_count; node_idx++) {
             Node node = nodepool[node_idx];
+
             if (node.val == 0) {
                 nodepool[node_idx].val = 1;
-                node_idx = node.next;
                 continue;
             }
 
             int digits = count_digits(node.val);
 
             if (digits % 2 == 0) {
-                long long front;
-                long long back;
+                unsigned long long front;
+                unsigned long long back;
                 split(node.val, digits, &front, &back);
-                nodepool[node_idx].val = front;
+                // nodepool[node_idx].val = front;
 
-                Node newnode;
-                newnode.val = back;
-                newnode.next = node.next;
-                nodepool[node_idx].next = freenode;
+                int found_front = -1;
+                int found_back = -1;
+                // check if the value we need to add already exists
+                for (int idx2 = 0; idx2 < freenode; idx2++) {
+                    if (nodepool[idx2].val == back) {
+                        // nodepool[idx2].count += nodepool[node_idx].count;
+                        found_back = idx2;
+                        break;
+                    }
+                    if (nodepool[idx2].val == front) {
+                        found_front = idx2;
+                        break;
+                    }
+                }
 
-                nodepool[freenode] = newnode;
-                freenode++;
+                if (found_front >= 0) {
+                    nodepool[node_idx].val = back;
+                    nodepool[found_front].count += nodepool[node_idx].count;
+                } else if (found_back >= 0) {
+                    nodepool[node_idx].val = front;
+                    nodepool[found_back].count += nodepool[node_idx].count;
+                } else {
+                    nodepool[node_idx].val = front;
+                    Node newnode;
+                    newnode.val = back;
+                    newnode.count = nodepool[node_idx].count;
+                    nodepool[freenode] = newnode;
+                    freenode++;
+                }
 
-                node_idx = newnode.next;
             } else {
                 nodepool[node_idx].val *= 2024;
-                node_idx = node.next;
             }
         }
     }
 
-    return 1;
+    unsigned long long count = 0;
+
+    for (int idx = 0; idx < freenode; idx++) {
+        count += nodepool[idx].count;
+    }
+
+    return count;
 }
 
 int main() {
@@ -105,8 +130,8 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    long long ans1 = part1(example1);
-    printf("answers : %lld  \n", ans1);
+    unsigned long long ans1 = part1(example1, 25);
+    printf("answers : %llu  \n", ans1);
 
     while (true) {
         gpio_put(LED_PIN, 1);
