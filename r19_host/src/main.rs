d84f575461279526d42d075458df416b2728d2be
marcvanheerden <11750833+marcvanheerden@users.noreply.github.com>
2024-12-19 12:31:36 +0200

    rust day 19 break out a function

--- r19_host/src/main.rs
+++ r19_host/src/main.rs
@@ -1,5 +1,47 @@
 use std::collections::VecDeque;
 
+fn match_pattern(pattern: &str, towels: &[Vec<u8>]) -> u64 {
+    let mut ways_to_solve_this_line = 0;
+    let mut queue: VecDeque<(Vec<u8>, u64)> = VecDeque::new();
+    let characters = pattern.chars().rev().map(|c| c as u8).collect();
+    queue.push_front((characters, 1));
+
+    while let Some((stub, count)) = queue.pop_front() {
+        if stub.is_empty() {
+            ways_to_solve_this_line += count;
+        }
+
+        for towel in towels.iter() {
+            if towel.len() <= stub.len()
+                && towel
+                    .iter()
+                    .zip(stub.iter().rev())
+                    .all(|(c1, c2)| *c1 == *c2)
+            {
+                let mut next_stub = stub.clone();
+                for _ in 0..towel.len() {
+                    let _ = next_stub.pop();
+                }
+
+                let mut found = false;
+                for (other_stub, other_count) in queue.iter_mut() {
+                    if *other_stub == next_stub {
+                        *other_count += count;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if !found {
+                    queue.push_back((next_stub, count));
+                }
+            }
+        }
+    }
+
+    ways_to_solve_this_line
+}
+
 fn matchymatchy(input: &str) -> (u64, u64) {
     let mut lines = input.lines();
 
@@ -14,48 +56,11 @@ fn matchymatchy(input: &str) -> (u64, u64) {
     let mut possible_ways = 0;
 
     for line in lines.skip(1) {
-        let mut ways_to_solve_this_line = 0;
-        let mut queue: VecDeque<(Vec<u8>, u64)> = VecDeque::new();
-        let characters = line.chars().rev().map(|c| c as u8).collect();
-        queue.push_front((characters, 1));
-
-        while let Some((stub, count)) = queue.pop_front() {
-            if stub.is_empty() {
-                ways_to_solve_this_line += count;
-            }
-
-            for towel in towels.iter() {
-                if towel.len() <= stub.len()
-                    && towel
-                        .iter()
-                        .zip(stub.iter().rev())
-                        .all(|(c1, c2)| *c1 == *c2)
-                {
-                    let mut next_stub = stub.clone();
-                    for _ in 0..towel.len() {
-                        let _ = next_stub.pop();
-                    }
-
-                    let mut found = false;
-                    for (other_stub, other_count) in queue.iter_mut() {
-                        if *other_stub == next_stub {
-                            *other_count += count;
-                            found = true;
-                            break;
-                        }
-                    }
-
-                    if !found {
-                        queue.push_back((next_stub, count));
-                    }
-                }
-            }
-        }
-
-        if ways_to_solve_this_line > 0 {
+        let possible_ways_of_matching = match_pattern(line, &towels);
+        if possible_ways_of_matching > 0 {
             possible_designs += 1;
         }
-        possible_ways += ways_to_solve_this_line;
+        possible_ways += possible_ways_of_matching;
     }
 
     (possible_designs, possible_ways)
