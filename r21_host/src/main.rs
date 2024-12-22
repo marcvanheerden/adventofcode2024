88bacea09952878736f8001093b59d0dfd0a8d7f
marcvanheerden <11750833+marcvanheerden@users.noreply.github.com>
2024-12-22 10:06:55 +0200

    switch to transition based approach, unclear if it works

--- r21_host/src/main.rs
+++ r21_host/src/main.rs
@@ -1,7 +1,5 @@
 use std::collections::{HashMap, VecDeque};
 
-type KeySequence = Vec<u8>;
-
 const NUMPAD: &str = "789
 456
 123
@@ -13,7 +11,7 @@ const DIRPAD: &str = "X^A
 #[derive(Debug)]
 struct InputDevice {
     layout: Vec<(u8, u8, u8)>,
-    cache: HashMap<(u8, u8), Vec<Vec<u8>>>,
+    cache: HashMap<(u8, u8), Vec<KeySequence>>,
 }
 
 impl InputDevice {
@@ -74,11 +72,11 @@ impl InputDevice {
         output
     }
 
-    fn paths(&self, start: (u8, u8), stop: (u8, u8)) -> Vec<KeySequence> {
+    fn paths(&self, start: (u8, u8), stop: (u8, u8), count: usize) -> Vec<KeySequence> {
         let mut queue = VecDeque::new();
         queue.push_front(vec![(start.0, start.1, b'x')]);
 
-        let mut paths = Vec::new();
+        let mut paths: Vec<KeySequence> = Vec::new();
         while !queue.is_empty() && paths.is_empty() {
             for _ in 0..queue.len() {
                 let task = queue.pop_front().unwrap();
@@ -88,7 +86,15 @@ impl InputDevice {
                     let mut moves: Vec<_> =
                         task.into_iter().skip(1).map(|(_y, _x, dir)| dir).collect();
                     moves.push(b'A');
-                    paths.push(moves);
+                    let moves = moves
+                        .windows(2)
+                        .map(|w| KeyTransition {
+                            from: w[0],
+                            to: w[1],
+                            count,
+                        })
+                        .collect();
+                    paths.push(KeySequence { trs: moves });
                     continue;
                 }
 
@@ -103,9 +109,9 @@ impl InputDevice {
         paths
     }
 
-    fn ways_to_move(&mut self, from_key: u8, to_key: u8) -> Vec<KeySequence> {
+    fn ways_to_move(&mut self, from_key: u8, to_key: u8, count: usize) -> Vec<KeySequence> {
         if let Some(output) = self.cache.get(&(from_key, to_key)) {
-            return output.to_vec();
+            return output.clone();
         }
 
         let from_loc = self
@@ -115,21 +121,69 @@ impl InputDevice {
             .find(to_key)
             .expect("looking for a key that doesn't exist");
 
-        let ways = self.paths(from_loc, to_loc);
+        let ways = self.paths(from_loc, to_loc, count);
         self.cache.insert((from_key, to_key), ways.clone());
         ways
     }
 }
 
-fn meta_step(device: &mut InputDevice, sequence: KeySequence) -> Vec<KeySequence> {
-    let mut full_sequence = vec![b'A'];
-    full_sequence.extend(sequence);
+#[derive(Debug, Clone, PartialEq)]
+struct KeyTransition {
+    from: u8,
+    to: u8,
+    count: usize,
+}
 
-    let sequence_parts: Vec<_> = full_sequence
-        .windows(2)
-        .map(|w| device.ways_to_move(w[0], w[1]))
+#[derive(Debug, Clone, PartialEq)]
+struct KeySequence {
+    trs: Vec<KeyTransition>,
+}
+
+impl KeySequence {
+    fn compress(&mut self) {
+        // combine same key transitions and sum their counts
+        // as long as the first and the last are unmoved then its fine
+        'outer: loop {
+            for early_idx in 0..(self.trs.len() - 1) {
+                for later_idx in (early_idx + 1)..(self.trs.len() - 1) {
+                    if self.trs[early_idx].from == self.trs[later_idx].from
+                        && self.trs[early_idx].to == self.trs[later_idx].to
+                    {
+                        self.trs[early_idx].count += self.trs[later_idx].count;
+                        self.trs.remove(later_idx);
+                        continue 'outer;
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    fn total(&self) -> usize {
+        self.trs.iter().map(|kt| kt.count).sum::<usize>() + 1
+    }
+}
+
+fn meta_step(device: &mut InputDevice, sequence: KeySequence) -> Vec<KeySequence> {
+    // add the starting transition from A to first key press
+    let starting_key = sequence.trs.first().unwrap().from;
+    let mut full_sequence = KeySequence {
+        trs: vec![KeyTransition {
+            from: b'A',
+            to: starting_key,
+            count: 1,
+        }],
+    };
+    full_sequence.trs.extend(sequence.trs);
+
+    // get the different key combinations for each transition
+    let sequence_parts: Vec<Vec<KeySequence>> = full_sequence
+        .trs
+        .into_iter()
+        .map(|tr| device.ways_to_move(tr.from, tr.to, tr.count))
         .collect();
 
+    // combine all the key combinations for the transitions
     let mut sequences: Vec<KeySequence> = Vec::new();
     for part in sequence_parts.into_iter() {
         if sequences.is_empty() {
@@ -143,7 +197,19 @@ fn meta_step(device: &mut InputDevice, sequence: KeySequence) -> Vec<KeySequence
                 part.iter()
                     .map(|p| {
                         let mut s_clone = s.clone();
-                        s_clone.extend(p);
+                        let mut next_to = b'A';
+
+                        if let Some(next_trs) = p.trs.first() {
+                            next_to = next_trs.from;
+                        }
+
+                        let joiner = KeyTransition {
+                            from: s.trs.last().unwrap().to,
+                            to: next_to,
+                            count: 1,
+                        };
+                        s_clone.trs.push(joiner);
+                        s_clone.trs.extend(p.trs.clone());
                         s_clone
                     })
                     .collect::<Vec<_>>()
@@ -154,25 +220,36 @@ fn meta_step(device: &mut InputDevice, sequence: KeySequence) -> Vec<KeySequence
     sequences
 }
 
-fn part1(passcode: &str) -> usize {
+fn propagate(passcode: &str, robots: usize) -> usize {
     let mut numpad = InputDevice::new(NUMPAD);
     let mut dirpad = InputDevice::new(DIRPAD);
 
     let initial_sequence: Vec<_> = passcode.chars().map(|c| c as u8).collect();
+    let initial_sequence = initial_sequence
+        .windows(2)
+        .map(|w| KeyTransition {
+            from: w[0],
+            to: w[1],
+            count: 1,
+        })
+        .collect();
+    let initial_sequence = KeySequence {
+        trs: initial_sequence,
+    };
 
     let mut sequences = meta_step(&mut numpad, initial_sequence);
 
-    sequences = sequences
-        .into_iter()
-        .flat_map(|s| meta_step(&mut dirpad, s))
-        .collect();
+    easy_display(&sequences);
+    for _ in 0..robots {
+        sequences = sequences
+            .into_iter()
+            .flat_map(|s| meta_step(&mut dirpad, s))
+            .collect();
+        sequences.iter_mut().for_each(|s| s.compress());
+        easy_display(&sequences);
+    }
 
-    sequences = sequences
-        .into_iter()
-        .flat_map(|s| meta_step(&mut dirpad, s))
-        .collect();
-
-    let min_key_seq = sequences.into_iter().map(|s| s.len()).min().unwrap();
+    let min_key_seq = sequences.into_iter().map(|s| s.total()).min().unwrap();
     let number_parts: usize = passcode
         .chars()
         .filter(|c| c.is_ascii_digit())
@@ -186,8 +263,8 @@ fn part1(passcode: &str) -> usize {
 fn easy_display(ks: &[KeySequence]) {
     println!("---------------------------------------------");
     for seq in ks.iter() {
-        for key in seq.iter() {
-            print!("{}", *key as char);
+        for key in seq.trs.iter() {
+            print!("{}{}x{} ", key.from as char, key.to as char, key.count);
         }
         println!();
     }
@@ -195,8 +272,15 @@ fn easy_display(ks: &[KeySequence]) {
 }
 
 fn main() {
-    let input = include_str!("../input.txt");
-    dbg!(input.lines().map(part1).sum::<usize>());
+    let input = include_str!("../example1.txt");
+    dbg!(input
+        .lines()
+        .take(1)
+        .map(|s| propagate(s, 0))
+        .sum::<usize>());
 }
 
 // Assumptions: adding additional moves will never be more efficient
+// TODOs:
+// Order all the middle trs and then deduplicate
+// Debug thist thing
